cf4e42bac01024080857885732c54aa5e9fccb50
Luan Coêlho de Souza <luan.souza@tjto.jus.br>
2023-08-01 17:23:00 -0300

    info: Removendo projeto frontend.

--- frontend/src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-black">
-      <div className="animate-pulse bg-blue-500 w-16 h-16 rounded-full">
-        Carregando
-      </div>
-    </div>
-  );
-}
