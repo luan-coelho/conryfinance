e0c502d85be145204f3b5e2344ee51bdd9229907
Luan Coêlho de Souza <luan.souza@tjto.jus.br>
2023-08-02 13:26:56 -0300

    feat: Sidebar

--- frontend/components/Sidebar/index.tsx
+++ frontend/components/Sidebar/index.tsx
@@ -1,6 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPiggyBank, faUser } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+
 export default function Sidebar() {
   return (
-    <aside className="bg-black text-white w-[300px] h-[400px] flex p-4 flex-col rounded-2xl">
+    <aside className="bg-[#101727] text-white w-[300px] flex p-4 flex-col rounded-2xl gap-2">
       <div className="flex gap-3 py-3">
         <div className="bg-white text-black rounded w-12 h-12 flex items-center justify-center">
           <span className="font-bold text-xl">CF</span>
@@ -15,13 +19,29 @@ export default function Sidebar() {
         <hr className="h-px bg-gray-500 border-0" />
       </div>
 
-      <div className="py-3">
-        <span className="text-sm text-gray-300 font-bold">FINANCES</span>
-        <ul className="my-2">
-          <li className="bg-white text-black px-2 py-1 rounded">
-            Orçamentos mensais
+      <div>
+        <span className="text-xs text-gray-300 font-medium">FINANCES</span>
+        <ul className="my-1">
+          <li className="bg-white text-gray-800 px-4 py-1 rounded">
+            <Link href={'/monthlybudgets'}>
+              <div className="flex items-center gap-2">
+                <FontAwesomeIcon icon={faPiggyBank} size="1x" />
+                <span>Orçamentos mensais</span>
+              </div>
+            </Link>
           </li>
         </ul>
+      </div>
+
+      <div>
+        <span className="text-xs text-gray-300 font-medium">USER</span>
+        <div className="bg-gray-100 text-gray-800 flex items-center gap-2 px-4 py-1 rounded">
+          <FontAwesomeIcon icon={faUser} size="1x" />
+          <div className="flex flex-col">
+            <span className="text-xs font-bold">LUAN COÊLHO</span>
+            <span className="text-xs">Desenvolvedor de Software</span>
+          </div>
+        </div>
       </div>
     </aside>
   );
